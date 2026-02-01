cc407a01d718c836ba44204e782bf7dbe3ca6e69
Flenarn <flenarn@hotmail.com>
2026-02-01 09:11:07 +0100

    chore: Cleanup.

--- src/runescipt-parser/ast/Scripts.ts
+++ src/runescipt-parser/ast/Scripts.ts
@@ -9,7 +9,6 @@ import { Parameter } from './Parameter';
 import { Statement } from './statement/Statement';
 import { Token } from './Token';
 
-
 /**
  * A script declaration containing the header and code of a script.
  *
@@ -26,87 +25,87 @@ import { Token } from './Token';
  * ```
  */
 export class Script extends Node {
-  public readonly trigger: Identifier;
-  public readonly name: Identifier;
-  public readonly isStar: boolean;
-  public readonly parameters?: Parameter[];
-  public readonly returnTokens?: Token[];
-  public readonly statements: Statement[];
+    public readonly trigger: Identifier;
+    public readonly name: Identifier;
+    public readonly isStar: boolean;
+    public readonly parameters?: Parameter[];
+    public readonly returnTokens?: Token[];
+    public readonly statements: Statement[];
 
-  constructor(
-    source: NodeSourceLocation,
-    trigger: Identifier,
-    name: Identifier,
-    isStar: boolean,
-    parameters: Parameter[] | undefined,
-    returnTokens: Token[] | undefined,
-    statements: Statement[]
-  ) {
-    super(source);
-    this.trigger = trigger;
-    this.name = name;
-    this.isStar = isStar;
-    this.parameters = parameters;
-    this.returnTokens = returnTokens;
-    this.statements = statements;
+    constructor(
+        source: NodeSourceLocation,
+        trigger: Identifier,
+        name: Identifier,
+        isStar: boolean,
+        parameters: Parameter[] | undefined,
+        returnTokens: Token[] | undefined,
+        statements: Statement[]
+    ) {
+        super(source);
+        this.trigger = trigger;
+        this.name = name;
+        this.isStar = isStar;
+        this.parameters = parameters;
+        this.returnTokens = returnTokens;
+        this.statements = statements;
 
-    this.addChild(trigger);
-    this.addChild(name);
+        this.addChild(trigger);
+        this.addChild(name);
 
-    if (parameters) {
-      this.addChildren(parameters);
+        if (parameters) {
+          this.addChildren(parameters);
+        }
+
+        if (returnTokens) {
+          this.addChildren(returnTokens);
+        }
+
+        this.addChildren(statements);
     }
 
-    if (returnTokens) {
-      this.addChildren(returnTokens);
+    /**
+     * Script name, including '*' suffix when applicable.
+     */
+    get nameString(): string {
+      return this.isStar ? `${this.name.text}*` : this.name.text;
     }
 
-    this.addChildren(statements);
-  }
+    accept<R>(visitor: AstVisitor<R>): R {
+      return visitor.visitScript(this);
+    }
 
-  /**
-   * Script name, including '*' suffix when applicable.
-   */
-  get nameString(): string {
-    return this.isStar ? `${this.name.text}*` : this.name.text;
-  }
+    hashCode(): number {
+      return JavaObjects.hash(
+        this.trigger,
+        this.name,
+        this.isStar,
+        this.parameters,
+        this.returnTokens,
+        this.statements
+      );
+    }
 
-  accept<R>(visitor: AstVisitor<R>): R {
-    return visitor.visitScript(this);
-  }
+    equals(other: unknown): boolean {
+      if (this === other) return true;
+      if (!(other instanceof Script)) return false;
+      return (
+          JavaObjects.equals(this.trigger, other.trigger) &&
+          JavaObjects.equals(this.name, other.name) &&
+          this.isStar === other.isStar &&
+          JavaObjects.equals(this.parameters, other.parameters) &&
+          JavaObjects.equals(this.returnTokens, other.returnTokens) &&
+          JavaObjects.equals(this.statements, other.statements)
+      );
+    }
 
-  hashCode(): number {
-    return JavaObjects.hash(
-      this.trigger,
-      this.name,
-      this.isStar,
-      this.parameters,
-      this.returnTokens,
-      this.statements
-    );
-  }
-
-  equals(other: unknown): boolean {
-    if (this === other) return true;
-    if (!(other instanceof Script)) return false;
-    return (
-      JavaObjects.equals(this.trigger, other.trigger) &&
-      JavaObjects.equals(this.name, other.name) &&
-      this.isStar === other.isStar &&
-      JavaObjects.equals(this.parameters, other.parameters) &&
-      JavaObjects.equals(this.returnTokens, other.returnTokens) &&
-      JavaObjects.equals(this.statements, other.statements)
-    );
-  }
-
-  toString(): string {
-    return new ToStringHelper(this)
-      .add("trigger", this.trigger)
-      .add("name", this.name)
-      .add("isStar", this.isStar)
-      .add("parameters", this.parameters)
-      .add("returnTokens", this.returnTokens)
-      .add("statements", this.statements)
-      .toString();
-  }
+    toString(): string {
+        return new ToStringHelper(this)
+          .add("trigger", this.trigger)
+          .add("name", this.name)
+          .add("isStar", this.isStar)
+          .add("parameters", this.parameters)
+          .add("returnTokens", this.returnTokens)
+          .add("statements", this.statements)
+          .toString();
+    } 
 }
