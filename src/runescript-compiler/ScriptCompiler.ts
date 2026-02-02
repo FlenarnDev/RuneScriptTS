ffbaa68e9933a8e5a08aaff23b5266ab46a77705
flenarn <smallmustard@gmail.com>
2026-02-02 16:20:43 +0100

    feat: Script compiler start.

---
+++ src/runescript-compiler/ScriptCompiler.ts
@@ -0,0 +1,372 @@
+import { DynamicCommandHandler } from './configuration/command/DynamicCommandHandler';
+import { SymbolLoader } from './configuration/SymbolLoader';
+import { SymbolTable } from './symbol/SymbolTable';
+import { TriggerManager } from './trigger/TriggerManager';
+import { TypeManager } from './type/TypeManager';
+import { DiagnosticsHandler, BaseDiagnosticsHandler } from './diagnostics/DiagnosticsHandler';
+import { ScriptWriter } from './writer/ScriptWriter';
+import { PointerHolder } from './pointer/PointerHolder';
+import path from 'path';
+import { PrimitiveType } from './type/PrimitiveType';
+import { CommandTrigger } from './trigger/CommandTrigger';
+import { MetaType } from './type/MetaType';
+import { WrappedType } from './type/wrapped/WrappedType';
+import { ScriptFile } from '../runescipt-parser/ast/ScriptFile';
+import { Diagnostics } from './diagnostics/Diagnostics';
+import { ParserErrorListener } from './ParserErrorListener';
+import { ScriptParser } from '../runescipt-parser/parser/ScriptParser';
+import { PreTypeChecking } from './semantics/PreTypeChecking';
+import { TypeChecking } from './semantics/TypeChecking';
+import { RuneScript } from './codegen/script/RuneScript';
+import { CodeGenerator } from './codegen/CodeGenerator';
+import { PointerChecker } from './codegen/script/config/PointerChecker';
+
+/**
+ * An entry point for compiling scripts.
+ */
+export class ScriptCompiler{
+
+    /**
+     * Log directly to console for now.
+     */
+    private readonly logger = console;
+
+    /**
+     * The paths that contain the source code.
+     */
+    private readonly sourcePaths: readonly string[];
+
+    /**
+     * The paths that contain source code that is (mostly) excluded.
+     */
+    private readonly excludePaths: readonly string[];
+
+    /**
+     * The root table that contains all global symbols.
+     */
+    private readonly rootTable: SymbolTable;
+
+    /**
+     * A list of [SymbolLoader]s called before attempting compilation.
+     */
+    private readonly symbolLoaders: SymbolLoader[] = [];
+
+    /**
+     * A mapping of command names to their handler implementation.
+     */
+    private readonly dynamicCommandHandlers = new Map<string, DynamicCommandHandler>();
+
+    /**
+     * The [TypeManager] for the compiler that is used for registering and looking up types.
+     */
+    public readonly types: TypeManager = new TypeManager();
+
+    /**
+     * The [TriggerManager] for the compiler that is used for registering and looking up script triggers.
+     */
+    public readonly triggers: TriggerManager = new TriggerManager();
+
+    /**
+     * Called after every step with all diagnostics that were collected during it.
+     */
+    public diagnosticsHandler: DiagnosticsHandler = ScriptCompiler.DEFAULT_DIAGNOSTICS_HANDLER;
+
+
+    constructor(
+        sourcePaths: string[],
+        excludePaths: string[],
+        private readonly scriptWriter: ScriptWriter,
+        private readonly commandPointers: Map<string, PointerHolder>
+    ) {
+        // Normalize paths.
+        this.sourcePaths = sourcePaths.map(p => this.normalizePath(p));
+        this.excludePaths = excludePaths.map(p => this.normalizePath(p));
+
+        // Register core types.
+        this.types.registerAll(PrimitiveType);
+        this.setupDefaultTypeCheckers();
+
+        // Register the command trigger.
+        this.triggers.registerTrigger(CommandTrigger);
+    }
+
+    /**
+     * Adds the core type checkers that the compiler depends on.
+     */
+    private setupDefaultTypeCheckers(): void {
+        // Allow anything to be assigned to any (top type).
+        this.types.addTypeChecker((left, _) => left === MetaType.Any);
+
+        // Allow nothing to be assigned to any (bottom type).
+        this.types.addTypeChecker((_, right) => right === MetaType.Nothing);
+
+        // Allow anything to be assigned to error to prevent error propagation.
+        this.types.addTypeChecker((left, right) => left === MetaType.Error || right === MetaType.Error);
+
+        // Basic checker where both types are equal.
+        this.types.addTypeChecker((left, right) => left === right);
+
+        // Checker for [MetaData.Script] that compares parameter and return types.
+        this.types.addTypeChecker((left, right) => 
+            left instanceof MetaType.Script &&
+            right instanceof MetaType.Script &&
+            left.trigger === right.trigger &&
+            this.types.check(left.parameterType, right.parameterType) &&
+            this.types.check(left.returnType, right.returnType)
+        );
+
+        // Checker for [MetaType.Hook] that compares the trigger list type.
+        this.types.addTypeChecker((left, right) =>
+            left instanceof MetaType.Hook &&
+            right instanceof MetaType.Hook &&
+            this.types.check(left.transmitListType, right.transmitListType)
+        );
+
+        // Checker for [WrappedType] that compares the inner types.
+        this.types.addTypeChecker((left, right) => 
+            left instanceof WrappedType &&
+            right instanceof WrappedType &&
+            left.constructor === right.constructor &&
+            this.types.check(left.inner, right.inner)
+        );
+    }
+
+
+    /**
+     * Adds [loader] to the list of symbol loaders to run pre-compilation. This
+     * can be used to load external symbols outside of scripts.
+     */
+    public addSymbolLoader(loader: SymbolLoader): void {
+        this.symbolLoaders.push(loader);
+    }
+
+    /**
+     * Adds a [DynamicCommandHandler] to the compiler with the given [name].
+     * See [DynamicCommandHandler] for information on implementation.
+     * 
+     * If a handler was registered for the [name] already an error is thrown.
+     */
+    public addDynamicCommandHandler(name: string, handler: DynamicCommandHandler): void {
+        if (this.dynamicCommandHandlers.has(name)) {
+            throw new Error(`A dynamic command handler with the name of '${name}' already exists.`);
+        }
+
+        this.dynamicCommandHandlers.set(name, handler);
+    }
+
+    /**
+     * Runs the compiler bo loading external symbols then actual
+     * running the compile process.
+     */
+    public run(ext: string): void {
+        this.loadSymbols();
+        this.compile(ext);
+    }
+
+    /**
+     * Calls all [SymbolLoader]s added to the compiler.
+     */
+    private loadSymbols(): void {
+        for (const symbolLoader of this.symbolLoaders) {
+            symbolLoader.load(this.rootTable, this);
+        }
+    }
+
+    /**
+     * Initializes the actual compile pipeline.
+     */
+    private compile(ext: string): void {
+        // 1) Parse all files.
+        const [parseSuccess, fileNodes] = this.parse(ext);
+        if (!parseSuccess) {
+            return;
+        } 
+
+        // 2) Analyze the nodes.
+        const analyzeSuccess = this.analyze(fileNodes);
+        if (!analyzeSuccess) {
+            return;
+        }
+
+        // 3) Generate code
+        const [codeGenSuccess, scripts] = this.codegen(fileNodes);
+        if (!codeGenSuccess) {
+            return;
+        }
+
+        // 4) Check pointers
+        const pointerCheckSuccess = this.checkPointers(scripts);
+        if (!pointerCheckSuccess) {
+            return;
+        }
+
+        // 5) Write scripts
+    }
+
+    /**
+     * Parses all files in the source path and returns the parsed AST nodes.
+     */
+    private parse(ext: string): [boolean, ScriptFile[]] {
+        const diagnostics = new Diagnostics();
+        const fileNodes: ScriptFile[] = [];
+        let fileCount = 0;
+
+        for (const sourcePath of this.sourcePaths) {
+            this.logger.debug(`Parsing files in ${sourcePath}.`);
+
+            // Recursively walk all files.
+            const files = this.walkTopDown(sourcePath);
+            for (const file of files) {
+                // Skip directories and non-script files.
+                if (!fs.statSync(file).isFile() || path.extname(file) !== `.${ext}`) {
+                    continue;
+                }
+
+                const start = performance.now();
+                const errorListener = new ParserErrorListener(file, diagnostics);
+                const node: ScriptFile | null = ScriptParser.createScriptFile(file, errorListener);
+                if (node) {
+                    fileNodes.push(node);
+                }
+                fileCount++;
+
+                const time = (performance.now() - start).toFixed(2);
+                this.logger.trace(`Parsed ${file} in ${time}ms.`);
+            }
+        }
+
+        this.logger.debug(`Parsed ${fileCount} files.`);
+        this.diagnosticsHandler.handleParse(diagnostics);
+
+        return [diagnostics.hasErrors() === false, fileNodes];
+    }
+
+    /**
+     * Runs all [files] through the semantic analysis pipeline. If there were any errors,
+     * the program will be halted with the exit code `1`.
+     */
+    private analyze(files: ScriptFile[]): boolean {
+        const diagnostics = new Diagnostics();
+
+        // Pre-type check: This adds all scripts to the symbol table for lookup in the next phase.
+        this.logger.debug("Starting pre-type checking.");
+        const preTypeStart = performance.now();
+
+        const preTypeChecking = new PreTypeChecking(this.types, this.triggers, this.rootTable, diagnostics);
+        for (const file of files) {
+            const fileStart = performance.now();
+            file.accept(preTypeChecking);
+            const fileTime = (performance.now() - fileStart).toFixed(2);
+            this.logger.trace(`Pre-type checked ${file.source.name} in ${fileTime}ms.`);
+        }
+
+        const preTypeCheckingTime = (performance.now() - preTypeStart).toFixed(2);
+        this.logger.debug(`Finished pre-type checking in ${preTypeCheckingTime}ms.`);
+
+        // Type check: This does all major type checking.
+        this.logger.debug("Starting type checking.");
+        const typeStart = performance.now();
+
+        const typeChecking = new TypeChecking(this.types, this.triggers, this.rootTable, this.dynamicCommandHandlers, diagnostics);
+        for (const file of files) {
+            const fileStart = performance.now();
+            file.accept(typeChecking);
+            const fileTime = (performance.now() - fileStart).toFixed(2);
+            this.logger.trace(`Type checked ${file.source.name} in ${fileTime}ms.`);
+        }
+
+        const typeCheckingTime = (performance.now() - typeStart).toFixed(2);
+        this.logger.debug(`Finished type checking in ${typeCheckingTime}ms.`);
+
+        // Call the diagnostics handler.
+        this.diagnosticsHandler.handleTypeChecking(diagnostics);        
+
+        return !diagnostics.hasErrors();
+    }
+
+    /**
+     * Runs all [files] through the code generator. Returns a list of all generated [RuneScript].
+     * If there were any errors, the program will be halted with exit code `1`.
+     */
+    private codegen(files: ScriptFile[]): [boolean, RuneScript[]] {
+        const diagnostics = new Diagnostics();
+
+        /**
+         * Run each file through the code generator and fetch the scripts from the generator
+         * and add to a list that we return.
+         */
+        const scripts: RuneScript[] = [];
+        this.logger.debug("Starting codegen.");
+        const codeGenStart = performance.now();
+        for (const file of files) {
+            const fileStart = performance.now();
+            const codeGen = new CodeGenerator(this.rootTable, this.dynamicCommandHandlers, diagnostics);
+            file.accept(codeGen);
+            scripts.push(...codeGen.scripts);
+            const fileTime = (performance.now() - fileStart).toFixed(2);
+            this.logger.trace(`Generated code for ${file.source.name} in ${fileTime}ms.`);
+        }
+        const codeGenTime = (performance.now() - codeGenStart).toFixed(2);
+        this.logger.debug(`Finished codegen in ${codeGenTime}ms.`);
+
+        // Call the diagnostics handler.
+        this.diagnosticsHandler.handleCodeGeneration(diagnostics);
+
+        return [diagnostics.hasErrors() === false, scripts];
+    }
+
+    private checkPointers(scripts: RuneScript[]): boolean {
+        if (this.commandPointers.size < 1) {
+            // Early return if there is no pointer information for any command.
+            return false;
+        }
+
+        const diagnostics = new Diagnostics();
+
+        const pointerChecker = new PointerChecker(diagnostics, scripts, this.commandPointers);
+        this.logger.debug("Starting pointer checking.");
+        const pointerCheckStart = performance.now();
+        pointerChecker.run();
+        const pointerCheckTime = (performance.now() - pointerCheckStart).toFixed(2);
+        this.logger.debug(`Finished pointer checking in ${pointerCheckTime}ms.`);
+
+        // Call the diagnostics handler.
+        this.diagnosticsHandler.handlePointerChecking(diagnostics);
+
+        return !diagnostics.hasErrors();
+    }
+
+    /**
+     * Runs all [scripts] through the [ScriptWriter].
+     */
+    private write(scripts: RuneScript[]) {
+        this.logger.debug("Starting script writing.");
+        const scriptWriterStart = performance.now();
+        // 
+    }
+
+    /**
+     * Helper functions
+     */
+    normalizePath(p: string): string {
+        return path.normalize(path.resolve(p));
+    }
+
+    /**
+     * Should match Kotlin's recursive depth-first iteration close enough.
+     */
+    private walkTopDown(dir: string): string[] {
+        let results: string[] = [];
+        for (const entry of fs.readdirSync(dir { withFileTypes: true })) {
+            const fullPath = path.join(dir, entry.name);
+            if (entry.isDirectory()) {
+                results = results.concat(this.walkTopDown(fullpath));
+            } else {
+                results.push(fullPath);
+            }
+        }
+        return results;
+    }
+
+    public static readonly DEFAULT_DIAGNOSTICS_HANDLER: DiagnosticsHandler = new BaseDiagnosticsHandler();
+}
