a2e1180e1b63eabe7567461cbd6fc95912c306c1
flenarn <smallmustard@gmail.com>
2026-02-05 13:36:05 +0100

    feat: `PlaceholderCommand`.

---
+++ src/serverscript-compiler/command/PlaceholderCommand.ts
@@ -0,0 +1,30 @@
+import { Opcode } from "../../runescript-compiler/codegen/Opcode";
+import { CodeGeneratorContext } from "../../runescript-compiler/configuration/command/CodeGeneratorContext";
+import { DynamicCommandHandler } from "../../runescript-compiler/configuration/command/DynamicCommandHandler";
+import { TypeCheckingContext } from "../../runescript-compiler/configuration/command/TypeCheckingContext";
+import { MetaType } from "../../runescript-compiler/type/MetaType";
+import { Type } from "../../runescript-compiler/type/Type";
+
+export class PlaceholderCommand implements DynamicCommandHandler {
+    constructor(
+        private readonly type: Type,
+        private readonly value: unknown
+    ){}
+
+    typeCheck(context: TypeCheckingContext): void {
+        context.checkArgumentTypes(MetaType.Unit);
+        context.expression.type = this.type;
+    }
+    generateCode?(context: CodeGeneratorContext): void {
+        context.lineInstruction(context.expression);
+        
+
+        if (typeof this.value === 'number') {
+            context.instruction(Opcode.PushConstantInt, this.value);
+        } else if (typeof this.value === 'string') {
+            context.instruction(Opcode.PushConstantString, this.value);
+        } else {
+            throw new Error(`Unsupported value: ${this.value}`);
+        }
+    }
+}
